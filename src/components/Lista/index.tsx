ffe338e90fe0c5c13d09cea9a5f01b06302c938b
Kelly Guimarans <kelly.guimarans@gmail.com>
2023-02-21 17:27:22 -0300

    feat: introduzindo estados

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,8 +1,9 @@
+import { useState } from 'react';
 import Item from './Item';
 import style from './Lista.module.scss';
 
 function Lista() {
-    const tarefas = [
+    const [tarefas, setTarefas] = useState([
         {
             tarefa: 'React',
             tempo: '02:00:00'
@@ -15,11 +16,15 @@ function Lista() {
             tarefa: 'Typescript',
             tempo: '03:00:00'
         }
-    ];
+    ]);
+
+
 
     return(
         <aside className={style.listaTarefas}>
-            <h2> Estudos do dia </h2>
+            <h2 onClick={() => {
+                setTarefas([...tarefas, {tarefa: "Estudar estado", tempo: "05:00:00"}])
+            }}> Estudos do dia </h2>
             <ul>
                 {tarefas.map((item) => (
                     <Item
