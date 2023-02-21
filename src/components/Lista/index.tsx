5f9f920eb9de5e307288e6a0fa8fd7ca080246ee
Kelly Guimarans <kelly.guimarans@gmail.com>
2023-02-21 17:14:47 -0300

    feat: criando o cronometro

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,3 +1,4 @@
+import Item from './Item';
 import style from './Lista.module.scss';
 
 function Lista() {
@@ -20,11 +21,11 @@ function Lista() {
         <aside className={style.listaTarefas}>
             <h2> Estudos do dia </h2>
             <ul>
-                {tarefas.map((item, index) => (
-                    <li key={index} className={style.item}>
-                        <h3> {item.tarefa} </h3>
-                        <span>{item.tempo}</span>
-                    </li>
+                {tarefas.map((item) => (
+                    <Item
+                        key={item.tarefa}
+                        {...item}
+                    />
                 ))}
             </ul>
         </aside>
