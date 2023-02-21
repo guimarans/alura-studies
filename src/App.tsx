ba07e9ed2922249c71d5b37b2638a3521c4b327f
Kelly Guimarans <kelly.guimarans@gmail.com>
2023-02-21 12:25:21 -0300

    feat: inclusao de componente formulario, lista e botao

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import Formulario from './components/Formulario';
+import Lista from './components/Lista';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Formulario />
+      <Lista />
     </div>
   );
 }
