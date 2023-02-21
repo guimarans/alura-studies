a6e06d21f8ef078e3d2669e2ac835762a9703c87
Kelly Guimarans <kelly.guimarans@gmail.com>
2023-02-21 15:14:39 -0300

    feat: conhecendo props

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import style from './Botao.module.scss';
 
-class Botao extends React.Component {
+class Botao extends React.Component<{ children: any }> {
     render() {
         return(
-            <button className={style.botao}> Botao </button>
+            <button className={style.botao}>{ this.props.children } </button>
         )
     }
 }
