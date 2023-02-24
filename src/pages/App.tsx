ee488f7139d64f69c08306cd72013ff5ed2f669f
Kelly Guimarans <kelly.guimarans@gmail.com>
2023-02-24 17:36:34 -0300

    feat: mostrando tarefa selecionada

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Cronometro from '../components/Cronometro';
 import Formulario from '../components/Formulario';
 import Lista from '../components/Lista';
@@ -7,10 +7,23 @@ import style from './App.module.scss';
 
 function App() {
   const [tarefas, setTarefas] = useState<ITarefa[]>([]);
+  const [selecionado, setSelecionado] = useState<ITarefa>();
+
+  function selecionaTarefa(tarefaSelecionada: ITarefa) {
+    setSelecionado(tarefaSelecionada);
+    setTarefas(tarefasAnteriores => tarefasAnteriores.map( tarefa => ({
+      ...tarefa, 
+      selecionado: tarefa.id === tarefaSelecionada.id ? true : false
+    })))
+  }
+
   return (
     <div className={style.AppStyle}>
       <Formulario setTarefas={setTarefas} />
-      <Lista tarefas={tarefas} />
+      <Lista 
+        tarefas={tarefas}
+        selecionaTarefa={selecionaTarefa}
+      />
       <Cronometro />
     </div>
   );
