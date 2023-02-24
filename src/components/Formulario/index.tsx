8f049d60501e34a4d229b92564c625ef125885bf
Kelly Guimarans <kelly.guimarans@gmail.com>
2023-02-24 15:18:21 -0300

    feat: adicionando uma tarefa

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,17 +1,33 @@
 import React from "react";
+import { iTarefa } from "../../types/tarefa";
 import Botao from "../Botao";
 import style from './Formulario.module.scss';
 
-class Formulario extends React.Component {
+class Formulario extends React.Component<{
+    setTarefas: React.Dispatch<React.SetStateAction<Array<iTarefa>>>
+}> {
+    state = {
+        tarefa: "",
+        tempo: "00:00"
+    }
+
+    adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
+        evento.preventDefault();
+        this.props.setTarefas(tarefasAntigas => [...tarefasAntigas, {... this.state }]);
+        
+        console.log('state: ', this.state);
+    }
     render() {
         return(
-            <form className={style.novaTarefa}>
+            <form className={style.novaTarefa} onSubmit={this.adicionarTarefa.bind(this)}>
                 <div className={style.inputContainer}>
                     <label htmlFor="tarefa"> Adicione um novo estudo </label>
                     <input 
                         type="text" 
                         name="tarefa" 
                         id="tarefa" 
+                        value={this.state.tarefa}
+                        onChange={evento => this.setState({...this.state, tarefa: evento.target.value})}
                         placeholder="O que vc quer estudar"
                         required 
                     />
@@ -22,13 +38,15 @@ class Formulario extends React.Component {
                         type="time"
                         step="1"
                         name="tempo"
+                        value={this.state.tempo}
+                        onChange={evento => this.setState({...this.state, tempo: evento.target.value})}
                         id="tempo"
                         min="00:00:00"
                         max="01:30:00"
                         required 
                     />
                 </div>
-                <Botao> Adicionar </Botao>
+                <Botao type={"submit"}> Adicionar </Botao>
             </form>
         )
     }
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,13 +1,17 @@
+import { useState } from 'react';
 import Cronometro from '../components/Cronometro';
 import Formulario from '../components/Formulario';
 import Lista from '../components/Lista';
+import { iTarefa } from '../types/tarefa';
 import style from './App.module.scss';
 
 function App() {
+  const [tarefas, setTarefas] = useState<Array<iTarefa> | []>([]);
+
   return (
     <div className={style.AppStyle}>
-      <Formulario />
-      <Lista />
+      <Formulario setTarefas={setTarefas} />
+      <Lista tarefas={tarefas} />
       <Cronometro />
     </div>
   );
