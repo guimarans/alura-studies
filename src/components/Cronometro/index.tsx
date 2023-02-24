22d45728cf97b468265bda69dc5107fc320fe994
Kelly Guimarans <kelly.guimarans@gmail.com>
2023-02-24 19:07:21 -0300

    feat: funcionalidades do cronometro

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -19,13 +19,22 @@ export default function Cronometro({selecionado}: Props) {
     }
   },[selecionado]);
 
+  function regressiva(contador: number = 0){
+    setTimeout(() => {
+      if(contador > 0) {
+        setTempo(contador - 1);
+
+        return regressiva(contador - 1);
+      }
+    }, 1000)
+  }
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o Cronômetro</p>
       <div className={style.relogioWrapper}>
         <Relogio tempo={tempo} />
       </div>
-      <Botao>
+      <Botao type="button" onClick={() => regressiva(tempo)}>
         Começar!
       </Botao>
     </div>
